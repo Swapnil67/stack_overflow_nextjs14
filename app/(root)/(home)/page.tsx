d81c9a86769e7d011ea0f62a3f6b3bbd9c947be0
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2023-12-28 20:26:26 +0530

    Created ask a question form and added validation

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,6 +5,7 @@ import LocalSearchBar from '@/components/shared/search/LocalSearchBar'
 import SelectFilters from '@/components/shared/select/SelectFilters';
 import { Button } from '@/components/ui/button'
 import { HomePageFilters } from '@/constants/filters';
+import Link from 'next/link';
 import React from 'react'
 
 
@@ -44,9 +45,11 @@ const Home = () => {
     <>
       <div className='flex w-full flex-col-reverse items-center justify-between gap-4 sm:flex-row sm:items-center' >
           <h1 className='h1-bold'>All Questions</h1>
-          <Button className='primary-gradient min-h-[46px] px-4 py-3 text-light-900'>
-            Ask a Question
-          </Button>
+          <Link href='/ask-question'>
+            <Button className='primary-gradient min-h-[46px] px-4 py-3 text-light-900'>
+              Ask a Question
+            </Button>
+          </Link>
       </div>
 
       <div className='mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
